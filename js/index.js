ab5d54ae8bc65c8ce826d746b4af893121b34701
tmoran18 <moran.tim01@gmail.com>
2020-07-12 22:44:23 +1000

    changed to Static

---
+++ js/index.js
@@ -0,0 +1,23 @@
+const MENU_LINKS = document.getElementById("menuLinks");
+const BURGER = document.querySelector(".hamburger_menu");
+const MEDIA_QUERY = window.matchMedia("(min-width: 700px)");
+
+const mobileNav = () => {
+  MENU_LINKS.className === "menu_links"
+    ? (MENU_LINKS.className += " mobile" + " transition")
+    : (MENU_LINKS.className = "menu_links");
+};
+
+const closeMobileNav = (MEDIA_QUERY) => {
+  if (MEDIA_QUERY.matches) {
+    MENU_LINKS.className = "menu_links";
+    BURGER.classList.remove("change");
+  }
+};
+
+BURGER.addEventListener("click", mobileNav);
+MEDIA_QUERY.addListener(closeMobileNav); // Attach listener function on state changes
+closeMobileNav(MEDIA_QUERY); // Call listener function at run time
+
+const animateBurger = (hamburger_menu) =>
+  hamburger_menu.classList.toggle("change");
