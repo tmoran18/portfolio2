59cafdb623d455b7168e10004964ca0d2e70c654
tmoran18 <moran.tim01@gmail.com>
2020-07-18 16:30:32 +1000

    nearly completed contact form hover states and honeypot todo

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 const MENU_LINKS = document.getElementById("menuLinks");
 const BURGER = document.querySelector(".hamburger_menu");
 const MEDIA_QUERY = window.matchMedia("(min-width: 700px)");
-const FORM = document.querySelector("form");
+const INPUTS = document.querySelectorAll(".input_container");
 
 const mobileNav = () => {
   MENU_LINKS.className === "menu_links"
@@ -23,8 +23,20 @@ closeMobileNav(MEDIA_QUERY); // Call listener function at run time
 const animateBurger = (hamburger_menu) =>
   hamburger_menu.classList.toggle("change");
 
-FORM.addEventListener("change", (e) => {
-  console.log(e.target);
+// Onload highlight the firstname input and first_name Label
+INPUTS.forEach((input) => {
+  input.childNodes[3].addEventListener("focus", (e) => {
+    resetActiveInputs();
+    e.currentTarget.classList.add("active_input");
+    e.currentTarget.previousElementSibling.classList.add("active_label");
+  });
 });
 
-// Onload highlight the firstname input and first_name Label
+const resetActiveInputs = () => {
+  INPUTS.forEach((input) => {
+    const INPUT = input.childNodes[3];
+    const LABEL = input.childNodes[1];
+    INPUT.classList = "active_input" && INPUT.classList.remove("active_input");
+    LABEL.classList = "active_label" && LABEL.classList.remove("active_label");
+  });
+};
