ec6de81aa82578af5907c6102e96304d6294749c
tmoran18 <moran.tim01@gmail.com>
2020-07-18 15:05:59 +1000

    working on contact form

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 const MENU_LINKS = document.getElementById("menuLinks");
 const BURGER = document.querySelector(".hamburger_menu");
 const MEDIA_QUERY = window.matchMedia("(min-width: 700px)");
+const FORM = document.querySelector("form");
 
 const mobileNav = () => {
   MENU_LINKS.className === "menu_links"
@@ -21,3 +22,9 @@ closeMobileNav(MEDIA_QUERY); // Call listener function at run time
 
 const animateBurger = (hamburger_menu) =>
   hamburger_menu.classList.toggle("change");
+
+FORM.addEventListener("change", (e) => {
+  console.log(e.target);
+});
+
+// Onload highlight the firstname input and first_name Label
